d9add0e44c091eb396ade12b4513c2f645b8e0e7
ThiagoThomas09 <thiagothomas09@outlook.com>
2023-12-12 16:35:25 -0300

    Adicionado lojas

--- loja/admin.py
+++ loja/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from . models import *
 
 admin.site.register(Cliente)
+admin.site.register(Loja)
 admin.site.register(Produto)
 admin.site.register(ListaDesejos)
--- loja/models.py
+++ loja/models.py
@@ -8,10 +8,27 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Loja(models.Model):
+    nome = models.CharField(max_length=100, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = 'static/images/default.jpg'
+        return url
     
 class Produto(models.Model):
     nome = models.CharField(max_length=150)
     preco = models.FloatField()
+    #Permitindo null temporariamente para migrar
+    loja = models.ForeignKey(Loja, on_delete=models.CASCADE, null=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
