a3131df7a2976b4ae6529aa1490f5d50d2da6e00
ThiagoThomas09 <thiagothomas09@outlook.com>
2024-01-10 13:48:44 -0300

    Adicionado as listas do usuário no admin em Users

--- lista_desejos/admin.py
+++ lista_desejos/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from . models import *
 
+class ListaDesejosInline(admin.TabularInline):
+    model = ListaDesejos
+    extra = 0
+    fields = ('nome', 'criado_em')
+    readonly_fields = ('nome', 'criado_em')
+
 class ItemListaDesejosInline(admin.TabularInline):
     model = ItemListaDesejos
     # define se vai ter campos vazios para adicionar novos itens
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from lista_desejos.admin import ListaDesejosInline
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    inlines = (ListaDesejosInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
